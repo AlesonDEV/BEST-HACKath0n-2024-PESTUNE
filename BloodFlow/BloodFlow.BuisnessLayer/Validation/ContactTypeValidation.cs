8011d9753bde423e306ad081996674b50eceed80
ViktorBoem <148382575+ViktorBoem@users.noreply.github.com>
2024-04-27 14:30:50 +0300

    Create validation

---
+++ BloodFlow/BloodFlow.BuisnessLayer/Validation/ContactTypeValidation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BloodFlow.BuisnessLayer.Validation
+{
+    public static class ContactTypeValidation
+    {
+        private static readonly Regex phoneRegex = new Regex(@"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$", RegexOptions.Compiled);
+
+        public static void ValidateEmail(string email)
+        {
+            BaseValidation.IsWhiteSpaceOrNullOrEmpty(email);
+
+            try
+            {
+                var mailAddress = new MailAddress(email);
+            }
+            catch (FormatException)
+            {
+                throw new ValidationException("Email format is invalid.");
+            }
+        }
+
+        public static void ValidatePhoneNumber(string phoneNumber)
+        {
+            BaseValidation.IsWhiteSpaceOrNullOrEmpty(phoneNumber);
+
+
+            if (!phoneRegex.IsMatch(phoneNumber))
+            {
+                throw new ValidationException("Phone number format is invalid.");
+            }
+        }
+    }
+}

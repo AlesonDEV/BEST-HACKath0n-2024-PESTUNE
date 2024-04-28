debd6c5ed380ec6274dfa510502a8e7b20af29a5
sejum-x <franchukivan123@gmail.com>
2024-04-28 09:25:02 +0300

    Profile Update | v0.4

--- lib/client/config/colors.dart
+++ lib/client/config/colors.dart
@@ -17,15 +17,10 @@ const CardColor1 = const Color(0xffa9b8ff);
 const SecondaryCardColor1 = const Color(0xff8690fc);
 const ImportantCardColor1 = const Color(0xffcfd8ff);
 
-
-
 const Color2 = const Color(0xff1f2c4b);
 const Color9 = const Color(0xffff4228);
 const Color11 = const Color(0xff16697a);
 const Color12 = const Color(0xffff284d);
-
-
-
 
 const Color1 = const Color(0xffc66d6d);
 const Color3 = const Color(0xffc09880);
